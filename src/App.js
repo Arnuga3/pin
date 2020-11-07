fd7bcac33903486cc6f4ca6eabe97f54a4e036c0
Arnuga3 <a.zelcs@rgu.ac.uk>
2020-11-07 19:42:31 +0000

    Do test commit from stackblitz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import 'semantic-ui-css/semantic.min.css';
 import PinGenerator from "./components/PinGenerator";
 
 export default function App() {
-  return (
-    <PinGenerator />
-  );
+  return <PinGenerator />;
 }
